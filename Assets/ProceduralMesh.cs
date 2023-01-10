c8b01ea204caadfef04924c650f4efafbb3b8efe
Lorna <eleonora.dups@gmail.com>
2023-01-10 22:46:27 +0100

    add unity procedural triangle

---
+++ Assets/ProceduralMesh.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+
+public class ProceduralMesh : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeMeshData();
+        CreateMesh();
+    }
+
+    void MakeMeshData()
+    {
+        //create array of vertices
+        vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0) };
+
+
+
+        //create array of integers
+        triangles = new int[] { 0, 1, 2 };
+    }
+
+    void CreateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
